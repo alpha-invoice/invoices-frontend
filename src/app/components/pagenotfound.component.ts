3e068140cd0354057412670cb1cee2aab2f5daad
Kristiyan Mechkov <kristiyan.mechkov@paysafe.com>
2016-09-09 16:54:54 +0300

    inital commit

---
+++ src/app/components/pagenotfound.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+/**
+ * Page not found error handler
+ */
+@Component({
+  templateUrl: 'templates/pagenotfound.component.html'
+})
+export class PageNotFoundComponent {
+}

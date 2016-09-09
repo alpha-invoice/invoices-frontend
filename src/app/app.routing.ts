d84d85229732bedfb4aa74cda3abb9744cc89659
Kristiyan Mechkov <kristiyan.mechkov@paysafe.com>
2016-09-09 12:17:46 +0300

    inital commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,19 @@
 
 import {RouterConfig, provideRouter} from "@angular/router";
+import {InvoiceListComponent} from "./components/invoice-list.component";
+import {InvoiceFormComponent} from "./components/invoice-form.component";
 export const routes: RouterConfig = [
-  {path: '', component:  HomePageComponent},
-  {path: 'home', component:  HomePageComponent},
-  {path: 'about', component: AboutPageComponent},
-  {path: 'contacts', component: ContactsPageComponent},
-  {path: 'features', component: FeaturesPageComponent},
-  {path: 'login', component: LoginPageComponent},
-  {path: 'profile',component: ProfilePageComponent},
-  {path: 'create/invoice', component: CreateInvoicePageComponent},
-  {path: 'settings', component: SettingsPageComponent},
-  {path: 'invoices', component: PreviousInvoicesPageComponent},
-  {path: 'logout', component: LogoutComponent}
+  // {path: '', component:  HomePageComponent},
+  // {path: 'home', component:  HomePageComponent},
+  // {path: 'about', component: AboutPageComponent},
+  // {path: 'contacts', component: ContactsPageComponent},
+  // {path: 'features', component: FeaturesPageComponent},
+  // {path: 'login', component: LoginPageComponent},
+  // {path: 'profile',component: ProfilePageComponent},
+   {path: 'create/invoice', component: InvoiceFormComponent},
+  // {path: 'settings', component: SettingsPageComponent},
+     {path: 'invoices', component: InvoiceListComponent},
+  // {path: 'logout', component: LogoutComponent}
 ];
 
 export const appRouterProvider = [provideRouter(routes)];

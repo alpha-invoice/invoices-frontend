3d4bc2a1205585ee764c201422e75b35f94adcaf
Mariya Dimitrova <mariya.dimitrova@paysafe.com>
2016-09-21 12:23:31 +0300

    Added buttom for downloading default template

--- src/app/components/main-page.component.ts
+++ src/app/components/main-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
+import {TemplateService} from "../services/template.service";
 
 /**
  *
@@ -8,8 +9,14 @@ import {ROUTER_DIRECTIVES} from "@angular/router";
   selector: 'main-page',
   templateUrl: 'templates/main-page.component.html',
   styleUrls: ['templates/styles/css/main-page.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+   providers: [TemplateService]
 })
 export class MainPageComponent {
-}
+  constructor(private templateService: TemplateService) {
+  }
 
+  getFile(event: any) {
+    this.templateService.getDefaultTemplate();
+  }
+}
